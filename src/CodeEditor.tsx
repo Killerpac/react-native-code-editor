3444eaa46175c9a87f4a3a2afa89ab057769a2a4
Killerpac <45239423+Killerpac@users.noreply.github.com>
2022-09-05 20:15:17 +0530

    commit

--- src/CodeEditor.tsx
+++ src/CodeEditor.tsx
@@ -278,7 +278,7 @@ const CodeEditor = (props: PropsWithForwardRef): JSX.Element => {
                 autoComplete="off"
                 autoCorrect={false}
                 autoFocus={autoFocus}
-                keyboardType="ascii-capable"
+                // keyboardType="ascii-capable"
                 editable={!readOnly}
                 testID={`${testID}-text-input`}
                 ref={inputRef}
